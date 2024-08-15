527a2a4dd158735a852805e2b102641630e85eab
Willow M <68018203+the-real-wkm@users.noreply.github.com>
2024-08-15 10:39:35 -0500

    Create dice-roller.php

---
+++ dice-roller.php
@@ -0,0 +1,18 @@
+<?php
+/*
+* Plugin Name:       WordPress Dice Roller
+* Plugin URI:        https://github.com/the-real-wkm/wpp-diceroller
+* Description:       Add a spot to "roll" your virtual dice on WordPress with ease!
+* Version:           0.0.1
+* Requires at least: 5.2
+* Requires PHP:      7.2
+* Author:            William Mercer
+* Author URI:        https://thefoxsgarden.net
+* License:           GPL v2 or later
+* License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+* Update URI:        https://github.com/the-real-wkm/wpp-diceroller
+* Text Domain:       wp-diceroller-plugin
+*/
+
+
+>
